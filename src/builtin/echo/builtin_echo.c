edee9e442764140caa6d72822f78ef05ec122eac
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-05-27 17:44:13 +0300

    feat(builtin/echo): add builtin echo command

---
+++ src/builtin/echo/builtin_echo.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 16:20:14 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/05/27 17:01:05 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/minishell.h"
+
+static bool	is_n_option(char *arg)
+{
+	int	i;
+
+	if (!arg && arg[0] != '-')
+		return (false);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (false);
+		i++;
+	}
+	return (i > 1);
+}
+
+int	builtin_echo(char **args)
+{
+	int		i;
+	bool	newline;
+
+	if (!args || !args[0])
+		return (1);
+	i = 1;
+	newline = true;
+	while (args[i] && is_n_option(args[i]))
+	{
+		newline = false;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+		i++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
+}
