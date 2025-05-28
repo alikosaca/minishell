14c0c8e0206557140ed5df5900021a21c977678c
alikosaca <alikosaca0@gmail.com>
2025-05-28 19:42:33 +0300

    feat(lexer): add token types and structure for lexer

--- include/lexer.h
+++ include/lexer.h
@@ -6,10 +6,33 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 15:23:35 by akosaca           #+#    #+#             */
-/*   Updated: 2025/05/21 15:25:19 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/05/28 19:36:11 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,				//normal kelimeler: ls, cat, file.txt
+	TOKEN_PIPE,				// |
+	TOKEN_REDIRECT_IN,		// <
+	TOKEN_REDIRECT_OUT,		// >
+	TOKEN_REDIRECT_APPEND,	// >>
+	TOKEN_HEREDOC,			// <<
+	TOKEN_SINGLE_QUOTE,		// 'text'
+	TOKEN_DOUBLE_QUOTE,		// "text"
+	TOKEN_ENV_VAR,			// $HOME, $USER
+	TOKEN_EOF,				// input sonu
+	TOKEN_ERROR,			// hatalı token
+}			t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;	// token türü
+	char			*value;	// token değeri
+	struct s_token	*next;	// sonraki token
+}				t_token;
+
 #endif
