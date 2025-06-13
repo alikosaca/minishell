2f6aa7147e4f39a626a351316986b2be3b698517
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-06-13 19:25:52 +0300

    style(src/env): reorder condition checks for consistency

--- src/env/env.c
+++ src/env/env.c
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:30:39 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/06/09 13:16:39 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/06/13 19:24:35 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	env(t_env *env_list)
 {
 	while (env_list)
 	{
-		if (env_list->exported && env_list->value)
+		if (env_list->value && env_list->exported)
 			printf("%s=%s\n", env_list->key, env_list->value);
 		env_list = env_list->next;
 	}
