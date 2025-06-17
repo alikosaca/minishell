6856a1b7a05dbe68156cf451ce5f8a9ba3cb1c53
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-06-18 02:28:19 +0300

    feat(src/parser): add command and redirect structs for shell parsing

---
+++ include/parser.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 00:05:52 by yaycicek          #+#    #+#             */
+/*   Updated: 2025/06/18 02:26:31 by yaycicek         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+typedef enum e_redirect_type
+{
+	REDIR_INPUT,        // <
+	REDIR_OUTPUT,       // >
+	REDIR_APPEND,       // >>
+	REDIR_HEREDOC       // <<
+}	t_redirect_type;
+
+typedef struct s_redirect
+{
+	t_redirect_type		type;         // redirection tipi
+	char				*file;        // hedef dosya adı
+	char				*limiter;     // heredoc limiter
+	struct s_redirect	*next;
+}	t_redirect;
+
+typedef struct s_command
+{
+	char				**argv;       // execve için argüman listesi (with NULL terminated)
+	t_redirect			*redirects;   // redirection listesi (last arg: NULL)
+	struct s_command	*next;        // pipe varsa bir sonraki komuta
+}	t_command;
+
+#endif
