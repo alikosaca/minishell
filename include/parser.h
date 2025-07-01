61e6b2a319eeaa172bb78f515cdee71afb826995
alikosaca <alikosaca0@gmail.com>
2025-07-01 21:55:08 +0300

    refactor(cmd): clean up cmd_handle logic and streamline cmd creation

--- include/parser.h
+++ include/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 00:05:52 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/06/27 11:34:55 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/07/01 21:50:36 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,17 @@
 # include "structs.h"
 # include "lexer.h"
 
-#endif
+t_cmd	*parser(t_token *tokens);
+t_cmd	*create_cmd(char *value, t_redirect *redirect);
 
 void	cmd_word(t_cmd **cmd, t_token *token);
+void	add_cmd(t_cmd **cmd, t_cmd *new_cmd);
+
+int	is_pipe(t_token_type type);
+int	is_redirect(t_token_type type);
+
+t_redirect_type	redirect_type(t_token_type *type);
+
+#endif
+
 
--- src/parser/cmd.c
+++ src/parser/cmd.c
@@ -6,33 +6,81 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/26 10:15:55 by akosaca           #+#    #+#             */
-/*   Updated: 2025/06/28 13:57:45 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/07/01 21:42:46 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parser.h"
 
-t_cmd	*create_cmd(char *value, t_redirect *redirect)
+int	argv_len(t_token *token)
 {
-	t_cmd	*cmd;
+	int	i;
+
+	i = 0;
+	if (!token)
+		return (0);
+	while (token->type == T_WORD)
+	{
+		i++;
+		token = token->next;
+	}
+	return (i);
+}
+
+t_cmd	*create_cmd(t_token *token, t_redirect *redirect)
+{
+	t_cmd	*new_cmd;
 	char	**argv;
+	int		len;
+	int		i;
 
-	cmd = malloc (sizeof(t_cmd));
-	if (!cmd)
+	i = 0;
+	len = argv_len(token);
+	argv = malloc(sizeof(char *) * (len + 1));
+	if (!argv)
 		return (NULL);
-	argv = ft_split(value, ' ');
-
-	cmd->argv = argv;
-	cmd->next = NULL;
+	while (len > i)
+	{
+		argv[i] = token->value;
+		token = token->next;
+		i++;
+	}
+	argv[i] = NULL;
+	new_cmd = malloc(sizeof(t_cmd));
+	if (!new_cmd)
+	{
+		free(argv);
+		return (NULL);
+	}
+	new_cmd->argv = argv;
+	new_cmd->next = NULL;
 	if (redirect)
 	{
-		cmd->redirects->type = redirect->type;
-		cmd->redirects->file = redirect->file;
-		cmd->next = NULL;
+		new_cmd->redirects = malloc(sizeof(t_redirect));
+		if (new_cmd->redirects)
+		{
+			new_cmd->redirects->type = redirect->type;
+			new_cmd->redirects->file = redirect->file;
+			new_cmd->redirects->next = NULL;
+			new_cmd->redirects = redirect;
+		}
 	}
 	else
+		new_cmd->redirects = NULL;
+	return (new_cmd);
+}
+
+void	add_cmd(t_cmd **cmd, t_cmd *new_cmd)
+{
+	t_cmd	*last_cmd;
+
+	if (!*cmd)
 	{
-		cmd->redirects = NULL;
+		*cmd = new_cmd;
+		return ;
 	}
-	return (cmd);
+	last_cmd = *cmd;
+	while (last_cmd->next)
+		last_cmd = last_cmd->next;
+	last_cmd->next = new_cmd;
 }
