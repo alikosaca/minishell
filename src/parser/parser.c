110c4024c7b6a542764fffd8c477b4dfa1f801bc
alikosaca <alikosaca0@gmail.com>
2025-07-04 19:43:34 +0300

    refactor(process_command): modify command processing logic

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,22 +6,30 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 07:54:15 by akosaca           #+#    #+#             */
-/*   Updated: 2025/07/01 12:52:06 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/07/04 18:42:04 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/parser.h"
 
-static void token_command(t_cmd **cmd, t_token *tokens)
+static void process_command(t_cmd **cmd, t_token *tokens)
 {
 	t_token	*val;
+	t_cmd	*new_cmd;
 
+	//cmd = malloc(sizeof(t_cmd));
 	val = tokens;
 	while (val)
 	{
+		new_cmd = init_cmd(&new_cmd);
 		if (val->type == T_WORD)
-			cmd_word(cmd, &tokens);
+			add_to_argv(new_cmd, &tokens);
+		else if (val->type == is_redirect(val->type))
+			if (val->next)
+				add_to_redirect(new_cmd, &tokens);
 		val = val->next;
+		add_cmd(cmd, new_cmd);
+		
 	}
 	
 }
