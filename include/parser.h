2f7658f1a1dedde34422800ad00578b14825205f
alikosaca <alikosaca0@gmail.com>
2025-07-04 19:49:48 +0300

    feat(parser): add function declarations to parser.h

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 00:05:52 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/07/01 21:50:36 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/07/04 19:48:13 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,14 @@
 
 # include "structs.h"
 # include "lexer.h"
+
+t_cmd	*init_cmd(t_cmd **cmd);
+char	**add_to_argv(t_cmd *cmd, t_token **tokens);
+int		argv_len(t_token *token);
+void	*add_to_redirect(t_cmd *cmd, t_token **tokens);
+t_redirect	*init_redirect(t_redirect **redirect);
+
+
 
 t_cmd	*parser(t_token *tokens);
 t_cmd	*create_cmd(char *value, t_redirect *redirect);
