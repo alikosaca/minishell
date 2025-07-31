177a4b407bdc4fd706de132a0b6cae4c69ed4f70
alikosaca <alikosaca0@gmail.com>
2025-07-31 21:30:35 +0300

    feat(terminal): implement proper PID handling - Manage terminal process ID tracking - Add PID validation checks - Ensure correct process group management

--- src/expansion/expansion_utils.c
+++ src/expansion/expansion_utils.c
@@ -6,27 +6,76 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 00:16:20 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/07/30 14:40:22 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/07/31 21:18:34 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../include/expansion.h"
 
+static char *find_process(char *str, int *i)
+{
+	int		status;
+	char	*res;
+	char	*tmp;
+
+	res = ft_strdup("");
+	status = 0;
+	while (str[(*i)] == '$')
+	{
+		status++;
+		(*i)++;
+		if (status == 2)
+		{
+			tmp = ft_strjoin(res, "42");
+			free(res);
+			res = tmp;
+			status = 0;
+		}
+	}
+	if (status == 1)
+	{
+		tmp = ft_strjoin(res, "$");
+		free(res);
+		res = tmp;
+	}
+	return (res);
+}
+
 char	*expand_dollar(t_shell *shell, char *str)
 {
 	char	*value;
+	char	*fp;
+	//char	*tmp;
+	int		i;
+	int		start;
+	char *var_name;
+	char *env_val;
 
 	if (!shell || !str)
 		return (NULL);
-	str++;
-	if (!(*str))
-		return (ft_strdup("$"));
-	if (ft_strncmp(str, "?", 1) == 0)
-		return (ft_itoa(shell->exitcode));
-	value = get_env_value(shell->envlist, str);
-	if (!value)
-		return (NULL);
-	return (ft_strdup(value));
+	i = 0;
+	fp = find_process(str, &i);
+	if (!str[i])
+		return (fp);
+	if (str[i] == '?')
+	{
+		value = ft_itoa(shell->exitcode);
+		i++;
+	}
+	else
+	{
+		start = i;
+		while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
+			i++;
+		var_name = ft_substr(str, start, i - start);
+		env_val = get_env_value(shell->envlist, var_name);
+		value = ft_strdup(env_val ? env_val : "");
+		free(var_name);
+	}
+	fp = ft_strjoin(fp, value);
+	//free(fp);
+	free(value);
+	return (fp);
 }
 
 // static int	var_len(char *str)
--- src/lexer/token_handlers.c
+++ src/lexer/token_handlers.c
@@ -6,7 +6,7 @@
 /*   By: akosaca <akosaca@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/22 18:38:30 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/07/22 14:25:06 by akosaca          ###   ########.fr       */
+/*   Updated: 2025/07/31 20:08:55 by akosaca          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,42 +38,92 @@ char	*handle_quote(char **input)
 	(*input)++;
 	return (result);
 }
+// static char	*get_len_dollar(char *input, int *i)
+// {
+// 	char	*res;
+// 	int		status;
+
+// 	status = 0;
+// 	res = NULL;
+// 	if (input[*i] == '$')
+// 		res = ft_strdup("42");
+// 	(*i)++;
+// 	while (input[*i] == '$')
+// 	{
+// 		status++;
+// 		(*i)++;
+// 		if (status == 2)
+// 		{
+// 			res = ft_strjoin(res, "42");
+// 			status = 0;
+// 		}
+// 	}
+// 	printf("res: %s\n", res);
+// 	if (status == 1)
+// 		(*i)--;
+// 	return (res);
+// }
+
+static char	*get_len_dollar(char *input, int *i)
+{
+	char	*res;
+	int		start_pos;
+
+	start_pos = (*i) - 1;
+	while (input[(*i)] == '$')
+		(*i)++;
+	res = malloc(sizeof(char) * ((*i - start_pos) + 1));
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, input+start_pos, (*i - start_pos) + 1);
+	res[*i - start_pos] = '\0';
+
+	return (res);
+}
 
 char	*handle_dollar(char **input)
 {
 	int		len;
 	char	*start;
 	char	*result;
+	int		i;
+	char	*dolar;
 
-	start = *input;
-	len = 1;
-
+	dolar = NULL;
+	i = 0;
 	(*input)++;
-	if (**input == '$')
-	{
-		(*input)++;
-		return (ft_strdup("$$"));
-	}
-	if (**input && !((ft_isalpha(**input) || **input == '_' || **input == '?')))
-		return (NULL);
-	else if ((!**input))
+	if ((!**input))
 		return (ft_strdup("$"));
+	else if (**input == '$')
+		dolar = get_len_dollar(*input, &i);
+
+	*input += i;
+	start = *input;
+	len = 0;
+	if (**input && !((ft_isalpha(**input) || **input == '_' || **input == '?')))
+		return (ft_strdup("$"));
+	else if ((!**input))
+		return (dolar);
 	else
 	{
-		(*input)++;
-		len++;
 		while (**input && (ft_isalnum(**input) || **input == '_'))
 		{
 			(*input)++;
 			len++;
 		}
 	}
-
 	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
 	ft_memcpy(result, start, len);
 	result[len] = '\0';
+	if (dolar != NULL)
+	{
+		char *temp = ft_strjoin(dolar, result);
+		free(dolar);
+        free(result);
+        return (temp);
+	}
 
 	return (result);
 }
