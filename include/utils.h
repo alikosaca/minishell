1f5103261b06ef0301b2d98ce545c02c507f7392
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-08-12 11:36:46 +0300

    feat(builtins/export): improve error messaging for invalid identifiers

--- include/utils.h
+++ include/utils.h
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:08:24 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/08/10 12:40:51 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/08/12 11:20:01 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void	__free(void ***ptr);
 void	cleanup(t_shell *shell);
 
 int		cmd_err(t_shell *shell, char *cmd, char *msg, int ret);
+int		exp_err(t_shell *shell, char *arg, char *msg, int ret);
 int		lx_err(t_shell *shell, char *sep);
 
 #endif
--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 20:15:16 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/08/12 10:35:18 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/08/12 11:22:32 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int	builtin_export(t_shell *shell, char **argv)
 				return (1);
 		}
 		else
-			return (cmd_err(shell, "export", ERR_NOT_A_VALID_IDENT, 1));
+			exp_err(shell, argv[i], ERR_NOT_A_VALID_IDENT, 1);
 		i++;
 	}
 	return (0);
