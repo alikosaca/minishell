91831e03a04f0fab77483f1c45f26e63be125ec7
yemreaycicek <yunus.e.aycicek@gmail.com>
2025-08-19 21:59:23 +0300

    fix(builtins/export): sort variables alphabetically and skip underscore variable

--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,24 +6,50 @@
 /*   By: yaycicek <yaycicek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 20:15:16 by yaycicek          #+#    #+#             */
-/*   Updated: 2025/08/16 21:03:04 by yaycicek         ###   ########.fr       */
+/*   Updated: 2025/08/19 21:49:40 by yaycicek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-static int	print_export_list(t_env *envlist)
+static t_env	*find_next_min(t_env *envlist, t_env *last)
 {
+	t_env	*min;
+
+	min = NULL;
 	while (envlist)
 	{
-		if (envlist->key && envlist->exported)
+		if (envlist->key && ft_strcmp(envlist->key, "_") == 0)
 		{
-			printf("declare -x %s", envlist->key);
-			if (envlist->value)
-				printf("=\"%s\"", envlist->value);
-			printf("\n");
+			envlist = envlist->next;
+			continue ;
+		}
+		if (envlist->key && envlist->exported
+			&& (!last || ft_strcmp(envlist->key, last->key) > 0)
+			&& (!min || ft_strcmp(envlist->key, min->key) < 0))
+		{
+			min = envlist;
 		}
 		envlist = envlist->next;
+	}
+	return (min);
+}
+
+static int	print_export_list(t_env *envlist)
+{
+	t_env	*last;
+	t_env	*min;
+
+	last = NULL;
+	min = find_next_min(envlist, last);
+	while (min)
+	{
+		printf("declare -x %s", min->key);
+		if (min->value)
+			printf("=\"%s\"", min->value);
+		printf("\n");
+		last = min;
+		min = find_next_min(envlist, last);
 	}
 	return (0);
 }
@@ -67,7 +93,7 @@ int	builtin_export(t_shell *shell, char **argv)
 		else
 		{
 			ret = 1;
-			exp_err(shell, argv[i], ERR_NOT_A_VALID_IDENT, 1);
+			exp_err(shell, argv[i], ERR_NOT_A_VALID_IDENT, ret);
 		}
 		i++;
 	}
